b00af2187a80ffc697f219828cff90f4f725d918
Delia-minjun <delia-minjun@outlook.com>
2026-05-12 08:29:54 +0800

    update

--- build/pick_place_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/pick_place_demo
-# Build directory: /root/catkin_ws/build/pick_place_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
